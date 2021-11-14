c930d72ced2a7130821aec38e76c6c5402257287
Jonathan Edward Carroll <jonathaniscarroll@gmail.com>
2021-11-14 13:13:43 -0400

    Intern Stuff!

---
+++ CubicleController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Events;
+[System.Serializable]
+public class CubicleEvent:UnityEvent<Cubicle>{
+	
+}
+
+public class CubicleController : MonoBehaviour
+{
+	public Cubicle Cubicle{
+		get{return cubicle;}set{cubicle = value;}
+	}
+	[SerializeField]
+	private Cubicle cubicle;
+	
+	
+	public List<CubicleObject> CubicleObjects{
+		get{
+			return cubicleObjects;
+		}
+		set{
+			cubicleObjects = value;
+			Cubicle.CubicleObjects = value;
+		}
+	}
+	[SerializeField]
+	private List<CubicleObject> cubicleObjects;
+	public CubicleEvent OutputCubicle;
+	public GameObjectEvent OutputWorkObjectEvent;
+	
+	public void Initilize(){
+		Cubicle = ScriptableObject.CreateInstance<Cubicle>();
+		Cubicle.CubicleObjects = CubicleObjects;
+		OutputCubicle.Invoke(Cubicle);
+	}
+	
+	public void OutputWorkObject(){
+		
+		GameObject output = Cubicle.CubicleObjects.OrderByDescending(i => i.Work).FirstOrDefault().gameObject;
+		Debug.Log("wokr object",output);
+		OutputWorkObjectEvent.Invoke(output);
+	}
+}
---
+++ CubicleObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubicleObject : MonoBehaviour
+{
+	public int Rest{
+		get{
+			return rest;
+		}
+		set{
+			rest = value;
+		}
+	}
+	[SerializeField]
+	private int rest;
+	
+	public int Work{
+		get{
+			return work;
+		}
+		set{
+			work = value;
+		}
+	}
+	[SerializeField]
+	private int work;
+	
+}
