80b8f8f72806b99bd8721de0b29189416f17a7b5
Jonathan Edward Carroll <jonathaniscarroll@gmail.com>
2021-11-16 01:13:00 -0400

    published build

---
+++ Scripts/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Post : ScriptableObject
+{
+	public string Content{
+		get{return _content;} set{_content = value;ContentUpdate.Invoke(_content);}
+	}
+	[SerializeField]
+	private string _content;
+	public StringEvent ContentUpdate = new StringEvent();
+	public int Likes{
+		get{return likes;}set{likes = value;LikeEvent.Invoke(likes);}
+	}
+	[SerializeField]
+	private int likes;
+	public IntEvent LikeEvent = new IntEvent();
+}
