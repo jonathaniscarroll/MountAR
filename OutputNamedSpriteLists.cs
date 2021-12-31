7beb41f9a07e253c402fc9293a7c709f74c52ee0
Jonathan Edward Carroll <jonathaniscarroll@gmail.com>
2021-12-31 16:45:40 -0400

    trying to figure out character creator

---
+++ OutputNamedSpriteLists.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutputNamedSpriteLists : MonoBehaviour
+{
+	[System.Serializable]
+	public class NamedSpriteList{
+		public string Name;
+		public SpriteListReference SpriteList;
+		
+	}
+	public List<NamedSpriteList> NamedSpriteLists;
+	public SpriteListEvent OutputSpriteList;
+	public void InputString(string input){
+		List<Sprite> output = NamedSpriteLists.Find(c => c.Name == input).SpriteList;
+		if(output.Count>0){
+			OutputSpriteList.Invoke(output);
+		}
+	}
+	
+}
---
+++ OutputStringList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutputStringList : MonoBehaviour
+{
+	public StringListReference StringList;
+	public StringListEvent StringListEvent;
+	public void OutputList(){
+		StringListEvent.Invoke(StringList);
+	}
+	public StringEvent IteratedStringEvent;
+	public void IterateList(int input){
+		string output = StringList.Value[input];
+		IteratedStringEvent.Invoke(output);
+	}
+	public IntEvent CountEvent;
+	public void OutputCount(){
+		CountEvent.Invoke(StringList.Value.Count);
+	}
+}
